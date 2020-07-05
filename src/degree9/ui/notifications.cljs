0714fe05c10cfe1d16196b24697e7615c0fa0f41
Matthew Ratzke <matt@degree9.io>
2020-07-05 10:22:17 -0600

    Create notifications.cljs

---
+++ src/degree9/ui/notifications.cljs
@@ -0,0 +1,18 @@
+(ns degree9.ui.notifications
+  (:require [uikit-hl.notification :as notify]))
+
+(def ^:private defaults {:timeout "2500"})
+
+(def notify! notify/notification)
+
+(defn primary! [opts]
+  (notify! (merge defaults opts {:status "primary"})))
+
+(defn success! [opts]
+  (notify! (merge defaults opts {:status "success"})))
+
+(defn warning! [opts]
+  (notify! (merge defaults opts {:status "warning"})))
+
+(defn danger! [opts]
+  (notify! (merge defaults opts {:status "danger"})))
