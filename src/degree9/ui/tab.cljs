a2a974146fc14c2e1a53e4503b230e6c0676c268
Matthew Ratzke <matt@degree9.io>
2021-06-25 14:56:52 -0600

    Update tab.cljs

--- src/degree9/ui/tab.cljs
+++ src/degree9/ui/tab.cljs
@@ -1,7 +1,6 @@
 (ns degree9.ui.tab
   (:require [hoplon.core :as h]
             [javelin.core :as j]
-            [uikit-hl.margin :as margin]
             [uikit-hl.tab :as tab]))
 
 (h/defelem tab [{:keys [items] :as attr} kids]
